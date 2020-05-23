69392389a3fbfcf480fbe8dab0461b5f00b5a687
Reverope <shatish123456789@gmail.com>
2020-05-23 13:02:34 +0530

    Added List

--- js/main.js
+++ js/main.js
@@ -1,5 +1,10 @@
 var path = document.getElementById("svg2");
 var stateName = document.getElementsByClassName("overview__heading");
+var timings = document.getElementsByClassName("av");
+
+setInterval(function () {
+  timings[0].innerHTML = Date().substring(0, 25);
+}, 1000);
 
 for (var i = 5; i < 77; i++) {
   path.childNodes[i].addEventListener("mouseover", (state) => {
@@ -7,7 +12,11 @@ for (var i = 5; i < 77; i++) {
 
     var stNm = document.getElementById(state.target["id"]);
 
+    stateName[0].style.opacity = "0";
     stateName[0].innerHTML = stNm.getAttribute("title");
+    setInterval(function () {
+      stateName[0].style.opacity = "1";
+    }, 500);
   });
   path.childNodes[i].addEventListener("mouseout", (state) => {
     document.getElementById(state.target["id"]).style.fill = "#efefef";
